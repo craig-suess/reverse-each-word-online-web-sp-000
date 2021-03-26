c4d25dce3933c28c23599270593b212ab9317b6f
Craig <suesscraig112715@gmail.com>
2021-03-26 16:33:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,6 +10,6 @@
 def reverse_each_word(string)
   new_string = [];
   string.split(" ").collect do|str|
-    new_string.join(" ") << str.reverse;
+    new_string.join(" ") << str.reverse();
   end 
 end 
