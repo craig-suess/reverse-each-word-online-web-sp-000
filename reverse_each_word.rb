fd6603e0f645c02d26608db83131df251bc47714
Craig <suesscraig112715@gmail.com>
2021-03-26 16:32:03 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+#def reverse_each_word(string)
+  
+#  new_string = [];
+#  string.split(" ").each do|str|
+#    new_string << str.reverse(); 
+#  end 
+#  new_string.join(" ");
+#end 
+
+def reverse_each_word(string)
+  new_string = [];
+  string.split(" ").collect do|str|
+    new_string.join(" ") << str.reverse;
+    string.join(" ")
+  end 
+end 
